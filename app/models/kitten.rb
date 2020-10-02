b50759fba31094c162973aaf5596eaaa5ce808de
Nico Rithner <nicorithner@gmail.com>
2020-10-01 20:59:26 -0600

    Create capitalize method in model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,10 @@
 class Kitten < ApplicationRecord
+  validates :name, :age, :cuteness, :softness, presence: true
+  before_create :capitalize
+
+  def capitalize
+    self.name.capitalize!
+    self.cuteness.capitalize!
+    self.softness.capitalize!
+  end
 end
