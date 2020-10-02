f5c039f560e9f84bea5e8f6f404f784f36024037
Nico Rithner <nicorithner@gmail.com>
2020-10-01 19:57:53 -0600

    Add 'create' action to controller with flash messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,4 +1,6 @@
 class KittensController < ApplicationController
+  include KittensHelper
+
   def index
     @kittens = Kitten.all
   end
@@ -9,5 +11,16 @@ class KittensController < ApplicationController
 
   def new
     @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.create(kitten_params)
+    if @kitten.save
+      flash[:sucess] = "#{@kitten.name} added successfully!"
+      redirect_to kitten_path(@kitten)
+    else
+      flash[:error] = kitten.errors.full_messages.to_sentence
+      render :new
+    end
   end
 end
